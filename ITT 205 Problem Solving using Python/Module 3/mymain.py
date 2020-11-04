ef605be1d476fb8bc2af45f9f89044bd373a2a53
Sarith <68165093+sarithdm@users.noreply.github.com>
2020-11-04 12:07:43 +0530

    Module

--- ITT 205 Problem Solving using Python/Module 3/mymain.py
+++ ITT 205 Problem Solving using Python/Module 3/mymain.py
@@ -1,3 +1,24 @@
 import mymodule
 mymodule.printer("Sum is")
 mymodule.sum(2,3)
+
+from mymodule import printer
+
+printer("Sum is with printer")
+
+from mymodule import *
+
+
+
+import mymodule
+x=0
+x=mymodule.counter(x)
+print(x)
+
+from imp import reload
+reload(mymodule)
+
+
+import mypackage
+mypackage.package1.fun1()
+mypackage.package2.fun2()
